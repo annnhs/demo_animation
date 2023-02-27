613e40dd9c33aa59ff780567b69454201ca3661f
annnhs <hoseong1218@gmail.com>
2023-02-27 17:01:07 +0900

    animate btn...

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -11,6 +12,18 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation(
+      'active',
+      autoplay: false,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,10 +49,86 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
           ),
           SafeArea(
-            child: Column(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 260,
+                    child: Column(
+                      children: const [
+                        Text(
+                          'Learn design & code',
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: 'Poppins',
+                            height: 1.2,
+                          ),
+                        ),
+                        SizedBox(height: 16,),
+                        Text(
+                          "Don't skip Design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
+                        ),
+                      ],
+                    ),
+                  ),
+                  AnimateBtn(
+                    btnAnimationController: _btnAnimationController,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+class AnimateBtn extends StatelessWidget {
+  const AnimateBtn({
+    Key? key,
+    required RiveAnimationController btnAnimationController,
+  }) : _btnAnimationController = btnAnimationController,
+        super(key: key);
+
+  final RiveAnimationController _btnAnimationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _btnAnimationController.isActive = true;
+      },
+      child: SizedBox(
+        height: 64,
+        width: 260,
+        child: Stack(
+          children: [
+            RiveAnimation.asset(
+              'assets/RiveAssets/button.riv',
+              controllers: [
+                _btnAnimationController
+              ],
+            ),
+            Positioned.fill(
+              top: 8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(CupertinoIcons.arrow_right),
+                  SizedBox(width: 8,),
+                  Text(
+                    'Start the course',
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
